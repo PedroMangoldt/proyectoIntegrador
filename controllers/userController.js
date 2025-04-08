0161198d514c30b3ac8b61938ff2262c8f15682a
ddukss <pignib@udesa.edu.ar>
2025-04-08 12:53:21 -0300

    Cambios en routes y controllers, falta modificar profile.ejs para que funcione correctamente

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,5 @@
+const data = require('../db/data');
+
 const userController = {
     index: function(req, res) {
         return res.send('Devuelve todos los usuarios');
@@ -5,7 +7,12 @@ const userController = {
     nombre: function(req,res) {
         let nombre = req.params.nombre;
         return res.send("Hola " + nombre)
-    }
+    },
+    profile: function(req, res) { 
+        return res.render('profile', { user: data.usuario });
+}
+
 }
 
 module.exports = userController
+
--- routes/users.js
+++ routes/users.js
@@ -7,3 +7,12 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+
+var express = require('express');
+var router = express.Router();
+const userController = require('../controllers/userController');
+
+router.get('/profile', userController.profile);
+
+module.exports = router;
