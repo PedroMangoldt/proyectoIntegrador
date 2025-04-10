e383efe3c06a5927c3157648fedec05280f9efab
PedroMangoldt <prodriguezmangoldt@udesa.edu.ar>
2025-04-10 20:23:39 -0300

    Seguimiento del proyecto

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,7 +2,11 @@ const data = require('../db/data');
 
 const indexController = {
     index: function(req, res) {
-        return res.render('index', {title: 'Mercado Liebre', productos: data.productos });
+        return res.render('index', {
+            title: 'Mercado Liebre', 
+            productos: data.productos, 
+            user: data.usuario
+        });
     }
 };
 
--- routes/register.js
+++ routes/register.js
@@ -4,4 +4,5 @@ const registerController = require("../controllers/registerController")
 
 router.get('/', registerController.register)
 
+
 module.exports = router;
