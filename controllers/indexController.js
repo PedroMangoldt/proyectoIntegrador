e61def5502cd3a434c8a722d2381d64eb9a6d1e0
PedroMangoldt <prodriguezmangoldt@udesa.edu.ar>
2025-04-13 18:02:47 -0300

    Seguimiento del trabajo. Agregue otro controlador

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,11 +2,7 @@ const data = require('../db/data');
 
 const indexController = {
     index: function(req, res) {
-        return res.render('index', {
-            title: 'Mercado Liebre', 
-            productos: data.productos, 
-            user: data.usuario
-        });
+        return res.render('index', {title: 'Mercado Liebre', productos: data.productos, user: data.usuario});
     }
 };
 
