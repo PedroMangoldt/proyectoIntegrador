bb6f468be0ad769b508c016fc3c80f6a497f65b1
itzjoaquinb <jbecerra@udesa.edu.ar>
2025-04-14 11:27:24 -0300

    Cambios Finales

--- routes/products.js
+++ routes/products.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var productController = require('../controllers/productController');
 
-router.get('/:id', productController.detalle);
+router.get('/detalle/:id', productController.detalle);
 router.get('/search-results', productController.searchResults);
 router.get('/productadd', productController.add);
 
