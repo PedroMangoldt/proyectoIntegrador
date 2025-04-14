4ed08780e6ee7f4d6e222f4efe3ea6166228392f
itzjoaquinb <jbecerra@udesa.edu.ar>
2025-04-13 22:41:24 -0300

    Cambios

--- db/data.js
+++ db/data.js
@@ -13,7 +13,7 @@ const data = {
       id: 1,
       nombre: 'Mouse gamer RGB',
       descripcion: 'Mouse con luces LED, diseño ergonómico y múltiples botones para gaming.',
-      imagen: '/images/products/mouseGamer.jpeg',
+      imagen: '/images/products/mouseGamer.png',
       comentarios: [
         {
           nombreUsuario: 'maria123',
@@ -31,7 +31,7 @@ const data = {
       id: 2,
       nombre: 'Mouse Pad extendido',
       descripcion: 'Alfombrilla para teclado y mouse con superficie antideslizante.',
-      imagen: '/images/products/mousePadGamer.png',
+      imagen: '/images/products/mousePadGame.png',
       comentarios: [
         {
           nombreUsuario: 'techie88',
@@ -44,7 +44,7 @@ const data = {
       id: 3,
       nombre: 'Parlantes estéreo gamer',
       descripcion: 'Parlantes con diseño gamer y sonido envolvente RGB.',
-      imagen: '/images/products/parlantesGamer.png',
+      imagen: '/images/products/parlantesGame.png',
       comentarios: [
         {
           nombreUsuario: 'ana_rock',
@@ -57,14 +57,14 @@ const data = {
       id: 4,
       nombre: 'Luces LED RGB',
       descripcion: 'Tira de luces LED para ambientar setups gamer con control remoto.',
-      imagen: '/images/products/lucesLedGamer.jpeg',
+      imagen: '/images/products/lucesLedGamer.png',
       comentarios: []
     },
     {
       id: 5,
       nombre: 'Escritorio gamer',
       descripcion: 'Escritorio con soporte para monitor, auriculares y estructura resistente.',
-      imagen: '/images/products/escritorioGamer.png',
+      imagen: '/images/products/escritorioGame.png',
       comentarios: [
         {
           nombreUsuario: 'sonicman',
@@ -77,21 +77,21 @@ const data = {
       id: 6,
       nombre: 'Silla gamer ergonómica',
       descripcion: 'Silla con soporte lumbar, reclinable y apoyabrazos ajustables.',
-      imagen: '/images/products/sillaGamer.png',
+      imagen: '/images/products/sillaGame.png',
       comentarios: []
     },
     {
       id: 7,
       nombre: 'Auriculares con micrófono',
       descripcion: 'Auriculares con sonido envolvente y micrófono retráctil.',
-      imagen: '/images/products/auricularesGamer.jpeg',
+      imagen: '/images/products/auricularesGamer.png',
       comentarios: []
     },
     {
       id: 8,
       nombre: 'Teclado mecánico RGB',
       descripcion: 'Teclado mecánico con iluminación LED y switches rojos.',
-      imagen: '/images/products/tecladoGamer.png',
+      imagen: '/images/products/tecladoGame.png',
       comentarios: [
         {
           nombreUsuario: 'musicfan',
@@ -104,14 +104,14 @@ const data = {
       id: 9,
       nombre: 'Compu Gamer RGB',
       descripcion: 'Gabinete con panel de vidrio templado y ventiladores RGB incluidos.',
-      imagen: '/images/products/cpuGamer.png',
+      imagen: '/images/products/cpuGame.png',
       comentarios: []
     },
     {
       id: 10,
       nombre: 'Monitor gamer 144Hz',
       descripcion: 'Monitor ultra ancho con alta tasa de refresco para juegos.',
-      imagen: '/images/products/monitorGamer.png',
+      imagen: '/images/products/monitorGame.png',
       comentarios: [
         {
           nombreUsuario: 'lucas_fit',
