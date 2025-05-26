e520f7da5a8381583337c5109a6c4ee904ac52e3
itzjoaquinb <jbecerra@udesa.edu.ar>
2025-05-26 12:36:46 -0300

    Cambios en el buscador para que filtre por la palabra escrita

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,6 @@
 const data = require('../db/data');
+const db = require("../database/models")
+
 
 const productController = {
     detalle: function (req, res) {
@@ -7,11 +9,32 @@ const productController = {
         return res.render('product', { producto });
     },
     searchResults: function (req, res) {
-        res.render("search-results", {resultados}); 
+        const busqueda = req.query.search;
+        const op = db.Sequelize.Op;
+
+        db.Product.findAll({
+            where: {
+                nombre: {[op.like]: `%${busqueda}%`}
+            },
+            include: [{association: "user"}]
+        })
+        .then(function(productos){
+        if(productos.length > 0){
+            res.render('search-results', {productos});
+        } 
+        else {
+            res.render("search-results", {productos: [], mensaje: `No se han encontrado resultados para ${busqueda}`})
+        }
+    })
+        .catch(function(error){
+            console.log(error);
+            res.send("Error al buscar productos")
+        })
     },
     add: function (req, res) {
         res.render("product-add");
-     }
+    },
+    
 };
 
 module.exports = productController;
