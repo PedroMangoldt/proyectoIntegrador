b59c36709c1bff5be5dcece641aee9301c5141bc
ddukss <pignib@udesa.edu.ar>
2025-05-30 13:55:06 -0300

    Punto 7 a revisar que nose porq no me anda el nodemon ahora, cambioe index controller trayendo los datos de req.session y agregue el href npara que los usuarios puedan aceder al perfil de quien publico el producto

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,10 +1,21 @@
 const data = require('../db/data');
 const db = require('../database/models');
 
-const indexController = {
+//Le cambie el nombre a la cost de indexController y lo hice segun el punto 7 abajo, lo dejo asi por las dudas por si llega a fallar algo para tener la estructura vieja, dsp la borro
+const AX = {
     index: function(req, res) {
         return res.render('index', {title: 'Mercado Liebre', productos: data.productos, user: data.usuario});
     }
+};
+
+const indexController = {
+    index: function(req, res) {
+        db.Product.findAll({
+            include: [{ association: "usuario" }]
+        })
+        .then(function(productos) {
+            return res.render('index', { productos: productos, user: req.session.user});
+        })}
 };
 
 module.exports = indexController;
