4564f2d4991113bf1b470f0c255e1daba8b50683
PedroMangoldt <prodriguezmangoldt@udesa.edu.ar>
2025-06-02 12:03:19 -0300

    Borre unas lineas en el index controller que estaban de mas

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,13 +1,6 @@
 const data = require('../db/data');
 const db = require('../database/models');
 
-//Le cambie el nombre a la cost de indexController y lo hice segun el punto 7 abajo, lo dejo asi por las dudas por si llega a fallar algo para tener la estructura vieja, dsp la borro
-const AX = {
-    index: function(req, res) {
-        return res.render('index', {title: 'Mercado Liebre', productos: data.productos, user: data.usuario});
-    }
-};
-
 const indexController = {
     index: function(req, res) {
         db.Product.findAll({
